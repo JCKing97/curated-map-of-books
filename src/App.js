81b2e7f5edd32df640bd04e3b909fc231979f3ce
JCKing97 <jck100notout@hotmail.co.uk>
2021-02-10 22:25:50 +0000

    Basic test of display json book data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import books from './data/books.json';
+import Book from './Book.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {books.map(b => <Book name={b.name} author={b.author} regions={b.regions} />)}
       </header>
     </div>
   );
---
+++ src/Book.js
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+function Book({name, author, regions = []}) {
+    return (
+        <div>
+            <h1>{name}</h1>
+            Author: {author}<br/>
+            Regions: {regions.join(", ")}
+        </div>
+    )
+}
+
+Book.propTypes = {
+    name: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    regions: PropTypes.string.isRequired
+};
+
+export default Book;
